fbba252ebc729e69c81f9fc3d211f41c0a5c46cf
asrafsujan247 <asraf.sujan247@gmail.com>
2025-02-27 21:42:08 +0600

    changed some codes in login js

--- js/login.js
+++ js/login.js
@@ -2,15 +2,16 @@ document
   .getElementById("btn-login")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    const numberInput = document.getElementById("number-input-field").value;
-    const pinInput = document.getElementById("pin-input-field").value;
-    if (numberInput.length === 11) {
-      if (parseInt(pinInput) === 1234) {
+    const number = document.getElementById("number-input-field").value;
+    const pin = document.getElementById("pin-input-field").value;
+    const convertedPin = parseInt(pin);
+    if (number.length === 11) {
+      if (convertedPin === 1234) {
         window.location.href = "../main.html";
       } else {
-        alert("Your Pin is not correct");
+        alert("Provide valid Pin");
       }
     } else {
-      alert("Need valid number");
+      alert("Provide your valid account Number");
     }
   });
